5ada6c2c558e7d91ea65da09cf850121e8510313
javaday <javaday@gmail.com>
2016-07-25 18:05:55 -0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/:name?', function (req, res) {
+	res.render('index', { name: req.params.name });
+});
+
+app.post('/', function (req, res) {
+	console.log(req.body);
+	res.send('Got a POST request');
+});
+
+app.listen(3000, function () {
+	console.log('Example app listening on port 3000!');
+});
